362f5c8f393fefd73b4d7c3fdcd75e20b7b6e1ad
Kamil <kam.nas21@wp.pl>
2022-05-16 15:00:05 +0200

    Working with JS accesibility and scss

--- script/app.js
+++ script/app.js
@@ -1,6 +1,9 @@
 (() => {
 	const dropdownArea = document.querySelector('.nav__list');
-
+	const hamburgerButton = document.querySelector('[data-hamburger]');
+	const navElement = document.querySelector('.nav');
+	const headerLinksElement = document.querySelector('.header__links');
+	const hamburgerButtonImage = document.querySelector('[data-icon]');
 	const changeAriaExpanded = target => {
 		const actualExpandedState = target.getAttribute('aria-expanded');
 		actualExpandedState === 'false'
@@ -30,17 +33,40 @@
 		});
 	};
 
+	const changeArrowDirection = target => {
+		const closestArrow = target.querySelector('[data-arrow]');
+		const actualExpandedState = target.getAttribute('aria-expanded');
+
+		actualExpandedState === 'false'
+			? closestArrow.setAttribute('src', './assets/images/icon-arrow-down.svg')
+			: closestArrow.setAttribute('src', './assets/images/icon-arrow-up.svg');
+	};
+
 	const showDropdown = target => {
 		if (!target.classList.contains('btn')) return;
-		changeAriaExpanded(target);
 		const dropdownID = target.getAttribute('aria-controls');
 		const dropdown = document.querySelector(`#${dropdownID}`);
 		dropdown.classList.toggle('u-active');
-
+		changeAriaExpanded(target);
+		changeArrowDirection(target);
 		managingFocus(dropdown);
+	};
+
+	const showMenu = () => {
+		navElement.classList.toggle('nav__mobile');
+		headerLinksElement.classList.toggle('u-flex');
+		headerLinksElement.classList.toggle('u-mobile-links');
+		hamburgerButtonImage.setAttribute(
+			'src',
+			`./assets/images/icon-${
+				navElement.classList.contains('nav__mobile') ? 'close-menu' : 'menu'
+			}.svg`
+		);
 	};
 
 	dropdownArea.addEventListener('click', ({ target }) => {
 		showDropdown(target);
 	});
+
+	hamburgerButton.addEventListener('click', showMenu);
 })();
